dd103a26a490c2dcdf7d4f43430d71602ea7ea3d
Wez Young <oldwezyoung@gmail.com>
2022-07-13 20:07:22 +0100

    Made current function asynchronous

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using api.Data;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -15,14 +16,14 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
         }
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }

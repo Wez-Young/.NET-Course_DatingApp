0ba2aca9038267ebd9f7a4fa6419f003f47390c6
Wez Young <oldwezyoung@gmail.com>
2022-07-19 11:59:26 +0100

    Implemented register user functionality

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,7 +18,7 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -26,7 +26,18 @@ export class AccountService {
     );
   }
 
-  setCurrentUser(user: User){
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
 

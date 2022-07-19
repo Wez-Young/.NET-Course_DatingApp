ff27a4f72be8ecbcee8423ed7982dc1f690f7dbe
Wez Young <oldwezyoung@gmail.com>
2022-07-19 11:54:47 +0100

    removed unecessary getUsers functionality

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHome: any;
+
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 

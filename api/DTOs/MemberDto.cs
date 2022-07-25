c1e6bebab063d2eff3bb4ba619a9872bb1572f36
Wez Young <oldwezyoung@gmail.com>
2022-07-25 15:51:12 +0100

    Added photoUrl prop

--- api/DTOs/MemberDto.cs
+++ api/DTOs/MemberDto.cs
@@ -9,7 +9,8 @@ namespace api.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
+        public string PhotoUrl { get; set; }
         public int Age { get; set; }
         public string Name { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
